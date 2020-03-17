11f7f97a0bcfe3d8adcf04e30e6441df84a03d52
Blackweather <szczepanski.karol0@gmail.com>
2020-03-17 10:33:15 +0100

    wip

--- generator.py
+++ generator.py
@@ -4,34 +4,83 @@ from selenium.webdriver.common.keys import Keys
 import time
 import random
 import subprocess
+import sys
+import os.path
 from requests import get
+
+def print_help():
+    print("This script will help you find random images on the web using" + 
+        " default random search terms or provided in file")
+    print("Running the script:")
+    print("python generator.py <args> / python3 generator.py <args>")
+    print("Command line arguments:")
+    print("-f/--file <filename> - the script will try to load search terms" + 
+        " from the provided file (see default.terms for example of file)")
+    print("-t/--time <number> - time of displaying a single image (in minutes)")
+    print("-h/--help - display this help")
+    print("Note: wrong arguments will result in displaying help")
+    print("Defaults:")
+    print("--file default.terms")
+    print("--time 5")
+
+def get_cmd_args():
+    if len(sys.argv) == 3 or len(sys.argv) == 5:
+        if sys.argv[1] == "--file" or sys.argv[1] == "-f":
+            # check file
+            pass
+        elif sys.argv[1] == "--time" or sys.argv[1] == "-t":
+            # check time
+            pass
+        else:
+            print("Wrong number of arguments, see usage below")
+            print_help()
+    elif len(sys.argv) == 1:
+        print("Running using the default arguments")
+    else:
+        print("Wrong number of arguments, see usage below:\n")
+        print_help()
+
+# reads the provided file and returns a list of terms
+def get_terms_from_file(filename = "default.terms"):
+    pass
+
+def download_from_url(url, filename):
+    pass
+
+def download_image():
+    pass
+
+def open_image(filename):
+    pass
+
+search_terms = [ 
+    "kotki",
+    "kot",
+    "petite cats",
+    "young cats",
+    "old cats",
+    "hairy cats",
+    "cute cats",
+    "slodkie kotki",
+    "kiciusie",
+    "kocieta",
+    "small cats",
+    "little cats",
+    "fat cats",
+    "homeless cats",
+    "norwegian cat",
+    "white cat",
+    "kociaki",
+    "persian cat",
+    "shorthair cat",
+    "british cat",
+    "cat stock photos"
+]
 
 while True:
     wd = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
     wd.maximize_window()
     wd.get('http://images.google.com/')
-    search_terms = [ 
-        "kotki",
-        "kot",
-        "petite cats",
-        "young cats",
-        "old cats",
-        "hairy cats",
-        "cute cats",
-        "slodkie kotki",
-        "kiciusie",
-        "kocieta",
-        "small cats",
-        "little cats",
-        "fat cats",
-        "homeless cats",
-        "norwegian cat",
-        "white cat",
-        "kociaki",
-        "persian cat",
-        "shorthair cat",
-        "british cat"
-    ]
 
     MAX_IMAGES = 11
 
